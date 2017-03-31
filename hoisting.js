f468e487413c5c1794ad10c20edb14ad1c3b83f9
Victoria Chuang <viiictoriachuang@gmail.com>
2017-03-31 17:42:55 -0500

    add code snippets demonstrating hoisting, scope and this

---
+++ hoisting.js
@@ -0,0 +1,46 @@
+// Javascript will hoist declarations of variables
+// to the top of their scopes, but not initializations
+
+const x = 7;
+const y = 5;
+console.log(x + ' ' + y); // Output: 7 5
+
+// This will not work, because y is initialized after it was used
+
+const x = 7;
+console.log(x + ' ' + y); // Error: y is not defined
+const y = 5;
+
+// The above is equivalent to the following
+
+const x = 7;
+let y;
+console.log(x + ' ' + y);
+y = 5;
+
+// Function declarations will be hoisted to the top of the scope
+hoist(); // Output: hoisted to the top
+
+function hoist() {
+  console.log('hoisted to the top');
+};
+
+// This will output undefined because
+// the assignment of a is not hoisted
+
+hoist(); // Output: undefined
+
+const a = "hello";
+function hoist() {
+  console.log(a);
+}
+
+// This will output hello because the definition
+// of a is available when hoist is called
+
+const a = "hello";
+hoist(); // Output: undefined
+
+function hoist() {
+  console.log(a);
+}
---
+++ variableScope.js
@@ -0,0 +1,35 @@
+// A variable's definition is available from its parent scope.
+
+var a = "kale";
+
+function scopedVariable() {
+  console.log(a);
+}
+
+scopedVariable(); // Output: kale
+
+// A global variable is able to be reassigned in a local scope.
+// Here, the value of a is accessible from inside scopedVariable
+
+var a;
+
+function scopedVariable() {
+  a = "hello"
+}
+
+console.log(a); // Output: undefined
+scopedVariable(); // The definition of a is now available to scopedVariable
+console.log(a); // Output: "hello"
+
+// The inclusion of var inside the local scope
+// prevents the globally-scoped a to be reassigned
+
+var a = "hello";
+
+function scopedVariable() {
+  var a = "kale"
+}
+
+console.log(a); // Output: "hello"
+scopedVariable();
+console.log(a); // Output: "hello"
